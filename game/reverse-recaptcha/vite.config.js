1324201d0c316644451a59bece657a354f4045c6
tk33r1 <tk33r1@gmail.com>
2026-03-29 23:17:03 +0900

    Fix: Add missing defineConfig import

--- game/reverse-recaptcha/vite.config.js
+++ game/reverse-recaptcha/vite.config.js
@@ -1,4 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/game/reverse-recaptcha/', // これが tk.st/game/reverse-recaptcha/ 用の住所になります
+  base: '/game/reverse-recaptcha/',
 })
