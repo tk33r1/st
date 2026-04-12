123043c236d9592990e2aa627c23bc8f45020208
tk33r1 <tk33r1@gmail.com>
2026-04-12 21:17:29 +0900

    update

---
+++ workers/wrangler/migrate.sql
@@ -0,0 +1,9 @@
+PRAGMA defer_foreign_keys=TRUE;
+INSERT INTO "scores" ("id","player_name","score","stage","created_at","game_id") VALUES(1,'test',93,1,'2026-04-12 08:42:24','masala-tetris');
+INSERT INTO "scores" ("id","player_name","score","stage","created_at","game_id") VALUES(2,'test',16,1,'2026-04-12 08:56:31','masala-tetris');
+INSERT INTO "scores" ("id","player_name","score","stage","created_at","game_id") VALUES(3,'test',45,2,'2026-04-12 09:17:37','masala-tetris');
+INSERT INTO "scores" ("id","player_name","score","stage","created_at","game_id") VALUES(4,'test',105,1,'2026-04-12 09:31:39','masala-tetris');
+INSERT INTO "scores" ("id","player_name","score","stage","created_at","game_id") VALUES(5,'test',98,1,'2026-04-12 09:33:58','masala-tetris');
+INSERT INTO "scores" ("id","player_name","score","stage","created_at","game_id") VALUES(6,'test',144,1,'2026-04-12 09:35:11','masala-tetris');
+INSERT INTO "scores" ("id","player_name","score","stage","created_at","game_id") VALUES(7,'test',24,1,'2026-04-12 09:36:33','masala-tetris');
+INSERT INTO "sqlite_sequence" ("name","seq") VALUES('scores',7);
